4aacb9541fa34fe7f67e6dbff424cae1b99702cc
mmurali12 <mutyalamurali1@gmail.com>
2022-11-03 20:07:42 +0530

    maven commited

---
+++ MaventestwithJenkins/src/main/java/jenkins/Mavenwithjenkins.java
@@ -0,0 +1,18 @@
+package jenkins;
+
+public class Mavenwithjenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry ="yes";
+		if(hungry == "yes") {
+			System.out.println("successfull");
+			
+		}
+		else {
+			System.out.println("full of hungry");
+		}
+
+	}
+
+}
